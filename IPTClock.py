0d96797c2abc68626566bbb842c772c4f9e0f528
Indomerun <joelm@chalmers.se>
2016-12-16 15:21:01 +0100

    Fix stages and change path to sponsors image.

--- IPTClock.py
+++ IPTClock.py
@@ -104,7 +104,7 @@ defaultBackgroundColor = None  # 'blue'    # String, following tkinter naming. c
 wedgeBackgroundColor = None  # '#13235b' #String, following matplotlib naming.  color of the wedge background (for example to adhere to present year's color scheme. None defaults to Tkinter color from defaultBackgroundColor
 clockColors = ['#7DC7EE', '#FED812', '#d32c2c', '#864da0']  # List of colors for the clock to cycle through
 
-leftSponsImagePath = './testPicture.gif'  # './ponyAndDuck.gif' # './Albin-300x286.gif'
+leftSponsImagePath = './sponsors.gif'  # './testPicture.gif'  # './ponyAndDuck.gif' # './Albin-300x286.gif'
 
 pathToSoundFile = './theDuckSong2.wav'  # If left empty nothing happens
 
